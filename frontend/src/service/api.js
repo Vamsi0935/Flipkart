61c699a61f5916de200bd09bcf893c23954a4635
Vamsi0935 <dvkrishna142000@gmail.com>
2024-08-27 09:42:18 +0530

    changes

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
-
 export const authenticateLogin = async (user) => {
   try {
-    const response = await axios.post(`${url}/login`, user);
+    const response = await axios.post(`http://localhost:5000/login`, user);
     return response.data;
   } catch (error) {
     console.error("Error while calling login API:", error);
@@ -14,7 +12,7 @@ export const authenticateLogin = async (user) => {
 
 export const authenticateSignup = async (user) => {
   try {
-    const response = await axios.post(`${url}/register`, user);
+    const response = await axios.post(`http://localhost:5000/register`, user);
     return response.data;
   } catch (error) {
     console.error("Error while calling signup API:", error);
